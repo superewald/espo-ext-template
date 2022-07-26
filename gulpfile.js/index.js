314cf267661594eb169c2d8d583571edb447e7b9
SuperEwald <code@superewald.net>
2022-07-26 14:34:54 +0200

    remove useless gulp scripts

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,11 +1,7 @@
-const { watch } = require('./tasks/watch')
-const { copy } = require('./tasks/copy')
 const { dist } = require('./tasks/distribute')
 const { init } = require('./tasks/init')
 
 module.exports = {
-    watch,
-    copy,
     dist,
     init
 }
