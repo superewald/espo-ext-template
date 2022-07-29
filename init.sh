37820bbfa60c78437d685647fbbeca00f01eda8a
SuperEwald <code@superewald.net>
2022-07-29 16:48:54 +0200

    fix init script

--- init.sh
+++ init.sh
@@ -54,17 +54,19 @@ EXT_AUTOLOAD_TPL='{
 }
 '
 
+echo "=> EspoCRM extension template initialization <="
+echo ""
 
 # read extension config
-echo "Please provide the following information: "
+echo "=> Please provide the following information: "
 
-read -p "Extension Name (eg: Awesome Extension): " extName
+read -p "=> Extension Name (eg: Awesome Extension): " extName
 if [[ "$extName" == "" ]]; then
     echo "ERROR: please provide extension name!"
     exit
 fi
 
-read -p "Extension Module Name (eg: AwesomeExtension): " extModuleName
+read -p "=> Extension Module Name (eg: AwesomeExtension): " extModuleName
 if [[ "$extModuleName" == "" ]]; then
     echo "ERROR: please provide module name!"
     exit
@@ -72,19 +74,19 @@ fi
 
 extModuleNameHyphen=$(sed --expression 's/\([A-Z]\)/-\L\1/g' --expression 's/^-//' <<< "$extModuleName")
 
-read -p "Extension Description: " extDescription
+read -p "=> Extension Description: " extDescription
 
-read -p "Extension Author Name (eg: SuperEwald) [git.user.name]: " extAuthorName
+read -p "=> Extension Author Name (eg: SuperEwald) [git.user.name]: " extAuthorName
 if [[ "$extAuthorName" == "" ]]; then
     extAuthorName=$(git config --global user.name)
 fi
 
-read -p "Extension Author Email [git.user.email]: " extAuthorEmail
+read -p "=> Extension Author Email [git.user.email]: " extAuthorEmail
 if [[ "$extAuthorEmail" == "" ]]; then
     extAuthorEmail=$(git config --global user.email)
 fi
 
-read -p "Extension Git Repository: " extRepository
+read -p "=> Extension Git Repository: " extRepository
 
 
 printf -v EXT_CONFIG_CONF "$EXT_CONFIG_TPL" "$extModuleName" "$extName" "$extDescription" "$extAuthorName" "$extAuthorEmail" "$extRepository"
@@ -98,6 +100,7 @@ echo "Writing infos to extension.json, package.json and composer.json"
 echo "$EXT_PACKAGE_CONF" > package.json
 echo "$EXT_CONFIG_CONF" > extension.json
 echo "$EXT_COMPOSER_CONF" > composer.json
+mkdir -p app/Resources
 echo "$EXT_AUTOLOAD_CONF" > app/Resources/autoload.json
 
 echo ""
@@ -108,6 +111,7 @@ if [[ "$currentOrigin" == *"superewald/espo-ext-template"* ]]; then
     git remote rename origin upstream
     git remote add origin "$extRepository"
     rm ./init.sh
+    git add .
     git commit -m "initial commit"
     git push -u origin main
 fi
